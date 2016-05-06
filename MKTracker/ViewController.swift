bb831b746b1e9409bf0ae44cc9b0965d9b98842f
Travis Thieman <travis.thieman@gmail.com>
2016-05-06 09:41:21 -0400

    Initial Commit

---
+++ MKTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MKTracker
+//
+//  Created by Travis Thieman on 5/6/16.
+//  Copyright © 2016 Travis Thieman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
